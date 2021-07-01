504472d452ccecbffd4e924aa31550dca07201e4
sirbromate <qWm)9of(BE,:f)Yf{@$MJ'_vb}H~WQ(_jZh>
2021-07-01 18:20:59 +0300

    Fixed frozen spruce generation. Added frozem spurce sapling.

---
+++ src/main/java/ru/sirbromate/magicmagic/block/FrozenTreeSupplingGenerator.java
@@ -0,0 +1,24 @@
+package ru.sirbromate.magicmagic.block;
+
+import net.minecraft.block.sapling.LargeTreeSaplingGenerator;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.TreeFeatureConfig;
+import org.jetbrains.annotations.Nullable;
+import ru.sirbromate.magicmagic.world.features.tree.ModFeatureTrees;
+
+import java.util.Random;
+
+public class FrozenTreeSupplingGenerator extends LargeTreeSaplingGenerator {
+
+    @Nullable
+    @Override
+    protected ConfiguredFeature<TreeFeatureConfig, ?> createLargeTreeFeature(Random random) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    protected ConfiguredFeature<TreeFeatureConfig, ?> createTreeFeature(Random random, boolean bees) {
+        return ModFeatureTrees.FROZEN_SPRUCE;
+    }
+}
